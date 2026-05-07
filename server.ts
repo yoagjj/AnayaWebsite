a98ba2ddd53d691041d68585c33143efc080a2ac
Julian <yogajuliansyaa@gmail.com>
2026-05-07 19:17:54 +0700

    fix: serve asset folder in production

--- server.ts
+++ server.ts
@@ -97,6 +97,7 @@ async function startServer() {
     app.use(vite.middlewares);
   } else {
     const distPath = path.join(__dirname, "dist");
+    app.use("/asset", express.static(path.join(__dirname, "asset")));
     app.use(express.static(distPath));
     app.get("*", (req, res) => {
       res.sendFile(path.join(distPath, "index.html"));
